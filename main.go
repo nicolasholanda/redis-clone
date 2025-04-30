7aeae4a03ffb4a0e02c8d43867772ebad9e4dd2f
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-04-29 22:09:20 -0300

    Added AOF

--- main.go
+++ main.go
@@ -9,12 +9,32 @@ import (
 func main() {
 	fmt.Println("Listening on port :6379")
 
-	// Create a new server at port 6379
+	// Create a new server
 	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+
+		handler(args)
+	})
 
 	// Listen for connections
 	conn, err := l.Accept()
@@ -24,8 +44,6 @@ func main() {
 	}
 
 	defer conn.Close()
-
-	fmt.Println("Redis clone is running on port 6379...")
 
 	for {
 		resp := NewResp(conn)
@@ -39,24 +57,28 @@ func main() {
 			fmt.Println("Invalid request, expected array")
 			continue
 		}
-	
+
 		if len(value.array) == 0 {
 			fmt.Println("Invalid request, expected array length > 0")
 			continue
 		}
-	
+
 		command := strings.ToUpper(value.array[0].bulk)
 		args := value.array[1:]
-	
+
 		writer := NewWriter(conn)
-	
+
 		handler, ok := Handlers[command]
 		if !ok {
 			fmt.Println("Invalid command: ", command)
 			writer.Write(Value{typ: "string", str: ""})
 			continue
 		}
-	
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
+		}
+
 		result := handler(args)
 		writer.Write(result)
 	}
